1d2b809d29ea16930477a098f95865e05dae3b1b
Yasser Abdelmonem <yasseramonem@gmail.com>
2021-07-13 16:48:18 +0200

    Error while trying to initiate  UnitTest

--- app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
+++ app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
@@ -61,7 +61,7 @@ class SleepDatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    suspend fun insertAndGetNight() {
+    suspend fun insertAndGetNight(){
         val night = SleepNight()
         sleepDao.insert(night)
         val tonight = sleepDao.getTonight()
